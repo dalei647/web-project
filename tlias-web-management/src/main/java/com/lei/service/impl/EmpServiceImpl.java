1a357f48615b2403d8ab5cd2e8f8745bd8deaa96
dalei647 <alei258000@gmail.com>
2026-01-11 21:04:31 +0800

    事务管理

--- tlias-web-management/src/main/java/com/lei/service/impl/EmpServiceImpl.java
+++ tlias-web-management/src/main/java/com/lei/service/impl/EmpServiceImpl.java
@@ -4,13 +4,12 @@ import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.lei.mapper.EmpExprMapper;
 import com.lei.mapper.EmpMapper;
-import com.lei.pojo.Emp;
-import com.lei.pojo.EmpExpr;
-import com.lei.pojo.EmpQueryParam;
-import com.lei.pojo.PageResult;
+import com.lei.pojo.*;
+import com.lei.service.EmpLogService;
 import com.lei.service.EmpService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
 
 import java.time.LocalDateTime;
@@ -24,8 +23,10 @@ public class EmpServiceImpl implements EmpService {
 
     @Autowired
     private EmpMapper empMapper;
-
+    @Autowired
     private EmpExprMapper empExprMapper;
+    @Autowired
+    private EmpLogService empLogService;
 
     /*@Override
     public PageResult page(Integer page, Integer pageSize, String name, Integer gender, LocalDate begin, LocalDate end) {
@@ -48,20 +49,30 @@ public class EmpServiceImpl implements EmpService {
         return new PageResult(p.getTotal(), p.getResult());
     }
 
+    @Transactional(rollbackFor = {Exception.class})
     @Override
     public void save(Emp emp) {
-        //1.补全基础属性
-        emp.setCreateTime(LocalDateTime.now());
-        emp.setUpdateTime(LocalDateTime.now());
-        //2.保存员工基本信息
-        empMapper.insert(emp);
+        try {
+            //1.补全基础属性
+            emp.setCreateTime(LocalDateTime.now());
+            emp.setUpdateTime(LocalDateTime.now());
 
-        //3. 保存员工的工作经历信息 - 批量
-        Integer empId = emp.getId();
-        List<EmpExpr> exprList = emp.getExprList();
-        if(!CollectionUtils.isEmpty(exprList)){
-            exprList.forEach(empExpr -> empExpr.setEmpId(empId));
-            empExprMapper.insertBatch(exprList);
+            //2.保存员工基本信息
+            empMapper.insert(emp);
+
+            int i = 1 / 0;
+
+            //3. 保存员工的工作经历信息 - 批量
+            Integer empId = emp.getId();
+            List<EmpExpr> exprList = emp.getExprList();
+            if (!CollectionUtils.isEmpty(exprList)) {
+                exprList.forEach(empExpr -> empExpr.setEmpId(empId));
+                empExprMapper.insertBatch(exprList);
+            }
+        } finally {
+            //记录操作日志
+            EmpLog empLog = new EmpLog(null, LocalDateTime.now(), emp.toString());
+            empLogService.insertLog(empLog);
         }
     }
 }
